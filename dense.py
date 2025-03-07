229a5b775035ec37e06e284fd98b7c262b33eb12
Jonas <jonas.buro1@gmail.com>
2025-03-06 21:41:22 -0800

    feat: custom dense layer implemented, 75% completion rate

--- dense.py
+++ dense.py
@@ -34,14 +34,42 @@ class Dense(Layer):
         self.trainable = True
         self.W = None
         self.w0 = None
+        self.W_opt = None
+        self.w0_opt = None
 
-    def forward_pass():
+    def initialize(self, optimizer):
+        self.W_opt = optimizer
+        self.w0_opt = optimizer
+
+        lim = 1.0/np.sqrt(self.input_shape[0])
+        self.W = np.random.uniform(
+            low=-lim, high=lim, size=(self.input_shape[0], self.n_units))
+        self.w0 = np.zeros((self.n_units,))
+
+    def parameters(self):
+        return self.W.size + self.w0.size
+
+    def forward_pass(self, X):
+        self.layer_input = X
+        return X @ self.W + self.w0
 
     def backward_pass(self, accum_grad):
+        W = self.W
+        grad_W = self.layer_input.T @ accum_grad
+        grad_w0 = np.sum(accum_grad, axis=0)
+        grad_input = accum_grad @ W.T
 
-        def number_of_parameters():
+        if self.trainable:
+            self.W = self.W_opt.update(self.W, grad_W)
+            self.w0 = self.w0_opt.update(self.w0, grad_w0)
 
-            # Initialize a Dense layer with 3 neurons and input shape (2,)
+        return grad_input
+
+    def output_shape(self):
+        return (self.n_units,)
+
+
+# Initialize a Dense layer with 3 neurons and input shape (2,)
 dense_layer = Dense(n_units=3, input_shape=(2,))
 
 # Define a mock optimizer with a simple update rule
