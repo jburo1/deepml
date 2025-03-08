706debcc32355fb6f3e372efa4d07f91162cda95
Jonas <jonas.buro1@gmail.com>
2025-03-07 21:13:12 -0800

    feat: rnn bptt, 75% completion rate

--- rnn_bptt.py
+++ rnn_bptt.py
@@ -1,45 +1,58 @@
 import numpy as np
-import copy
 
 
 class SimpleRNN:
     def __init__(self, input_size, hidden_size, output_size):
         self.hidden_size = hidden_size
-        self.W_xh = np.random.randn(hidden_size, input_size)*0.01
-        self.W_hh = np.random.randn(hidden_size, hidden_size)*0.01
-        self.W_hy = np.random.randn(output_size, hidden_size)*0.01
+        self.W_xh = np.random.randn(hidden_size, input_size) * 0.01
+        self.W_hh = np.random.randn(hidden_size, hidden_size) * 0.01
+        self.W_hy = np.random.randn(output_size, hidden_size) * 0.01
         self.b_h = np.zeros((hidden_size, 1))
         self.b_y = np.zeros((output_size, 1))
-        self.hts = []
-        self.yts = []
 
     def forward(self, x):
-        self.hts.clear()
-        self.yts.clear()
-        ht = np.zeros((self.hidden_size,))
-        for xt in x:
-            ht = np.tanh(self.W_xh@xt + self.W_hh@ht + self.b_h)
-            yt = self.W_hy@ht + self.b_y
-            self.hts.append(ht.copy())
-            self.yts.append(yt.copy())
-        return self.yts
+        h = np.zeros((self.hidden_size, 1))
+        outputs = []
+        self.last_inputs = []
+        self.last_hiddens = [h]
+
+        for t in range(len(x)):
+            self.last_inputs.append(x[t].reshape(-1, 1))
+            h = np.tanh(
+                self.W_xh @ self.last_inputs[t] + self.W_hh @ h + self.b_h)
+            y = self.W_hy @ h + self.b_y
+            outputs.append(y)
+            self.last_hiddens.append(h)
+
+        self.last_outputs = outputs
+        return np.array(outputs)
 
     def backward(self, x, y, learning_rate):
+        dW_xh = np.zeros_like(self.W_xh)
+        dW_hh = np.zeros_like(self.W_hh)
+        dW_hy = np.zeros_like(self.W_hy)
+        db_h = np.zeros_like(self.b_h)
+        db_y = np.zeros_like(self.b_y)
 
-        pass
+        dh_next = np.zeros((self.hidden_size, 1))
 
+        for t in reversed(range(len(x))):
+            dy = self.last_outputs[t] - \
+                y[t].reshape(-1, 1)
+            dW_hy += dy @ self.last_hiddens[t+1].T
+            db_y += dy
 
-input_sequence = np.array([[1.0], [2.0], [3.0], [4.0]])
-expected_output = np.array([[2.0], [3.0], [4.0], [5.0]])
-# Initialize RNN
-rnn = SimpleRNN(input_size=1, hidden_size=5, output_size=1)
+            dh = self.W_hy.T @ dy + dh_next
+            dh_raw = (1 - self.last_hiddens[t+1] ** 2) * dh
 
-# Forward pass
-output = rnn.forward(input_sequence)
+            dW_xh += dh_raw @ self.last_inputs[t].T
+            dW_hh += dh_raw @ self.last_hiddens[t].T
+            db_h += dh_raw
 
-# Backward pass
-rnn.backward(input_sequence, expected_output, learning_rate=0.01)
+            dh_next = self.W_hh.T @ dh_raw
 
-print(output)
-
-# The output should show the RNN predictions for each step of the input sequence.
+        self.W_xh -= learning_rate * dW_xh
+        self.W_hh -= learning_rate * dW_hh
+        self.W_hy -= learning_rate * dW_hy
+        self.b_h -= learning_rate * db_h
+        self.b_y -= learning_rate * db_y
